390da8842562a546b8a36435cfc6fc5a5a7dfa0e
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2017-10-27 16:10:30 +0800

    手数必须大于0

--- src/main/java/com/bohai/subAccount/constant/CommonConstant.java
+++ src/main/java/com/bohai/subAccount/constant/CommonConstant.java
@@ -4,39 +4,41 @@ package com.bohai.subAccount.constant;
  * @author caojia
  */
 public class CommonConstant {
-	
-	//警告弹出框
-	public static final String MESSAGE_BOX_WARN = "警告";
+    
+    //警告弹出框
+    public static final String MESSAGE_BOX_WARN = "警告";
 
-	//提示弹出框
-	public static final String MESSAGE_BOX_NOTICE = "提示";
-	
-	//错误弹出框
-	public static final String MESSAGE_BOX_ERROR = "错误";
-	
-	//确认弹出框
-	public static final String MESSGAE_BOX_CONF = "确认";
-	
-	//message
-	public static final String SX001C = "是否需要强制平仓？";
-	public static final String SX002C = "是否需要一键撤单？";
-	public static final String SX003C = "是否需要限制开仓？";
-	public static final String SX004C = "是否需要取消限制？";
-	
-	public static final String DIRECTION_MISS = "请选择买卖方向";
-	
-	public static final String CONTRACT_MISS = "请选择一个合约";
+    //提示弹出框
+    public static final String MESSAGE_BOX_NOTICE = "提示";
+    
+    //错误弹出框
+    public static final String MESSAGE_BOX_ERROR = "错误";
+    
+    //确认弹出框
+    public static final String MESSGAE_BOX_CONF = "确认";
+    
+    //message
+    public static final String SX001C = "是否需要强制平仓？";
+    public static final String SX002C = "是否需要一键撤单？";
+    public static final String SX003C = "是否需要限制开仓？";
+    public static final String SX004C = "是否需要取消限制？";
+    
+    public static final String DIRECTION_MISS = "请选择买卖方向";
+    
+    public static final String CONTRACT_MISS = "请选择一个合约";
 
-	public static final String PRICE_MISS = "请输入价格";
-	
-	public static final String OFFSET_MISS = "请选择开平标志";
-	
-	public static final String CONNECT_FIELD = "连接服务器失败";
-	
-	public static final String GET_PRICE_FIELD = "获取价格失败";
-	
-	public static final String VOLUME_MISS = "请输入数量";
-	
-	public static final String CLOSE_FIELD = "清仓失败";
-	
+    public static final String PRICE_MISS = "请输入价格";
+    
+    public static final String OFFSET_MISS = "请选择开平标志";
+    
+    public static final String CONNECT_FIELD = "连接服务器失败";
+    
+    public static final String GET_PRICE_FIELD = "获取价格失败";
+    
+    public static final String VOLUME_MISS = "请输入数量";
+    
+    public static final String VOLUME_LIMIT = "交易数量必须大于0";
+    
+    public static final String CLOSE_FIELD = "清仓失败";
+    
 }
